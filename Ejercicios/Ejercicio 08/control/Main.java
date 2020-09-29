7c1ea54e33944904738b5cbd8766f0b86ddb8b4d
45257012 <luqh555@gmail.com>
2020-09-29 19:34:17 +0200

    Ejercicio 8

---
+++ Ejercicios/Ejercicio 08/control/Main.java
@@ -0,0 +1,23 @@
+package control;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		char x = 'a';
+		for(int i =0;i<26;i++)
+		
+			System.out.print((char)(x+i)+" ");
+	
+
+
+
+
+	 
+}
+}
